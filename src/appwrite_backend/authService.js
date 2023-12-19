3f02907b7c511c5c7d1b2c170120445ad01f96f2
RahulRSoni <114871652+RahulRSoni@users.noreply.github.com>
2023-12-19 16:44:21 +0530

    add input, button, component

--- src/appwrite_backend/authService.js
+++ src/appwrite_backend/authService.js
@@ -46,7 +46,6 @@ export class AuthService {
 		} catch (error) {
 			throw error;
 		}
-		return null;
 	}
 
 	async logOut() {
--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,9 @@
 import Footer from './Footer/Footer';
 import Header from './Header/Header';
+import Container from './container/Container.jsx';
+import Logo from './Logo.jsx';
+import LogoutBtn from './Header/LogoutBtn.jsx';
+import Button from './button/button.jsx';
+import Input from './Input/Input.jsx';
 
-export { Footer, Header };
+export { Input, Button, Footer, Header, Container, Logo, LogoutBtn };
